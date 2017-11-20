2da3befd8061cd76db8aa0fe3593333c0b95cbbb
Sven Tryding <sven@tryding.se>
2017-11-20 21:47:45 +0100

    Dont expose PigW

--- pig/run_both.sh
+++
@@ -1,6 +0,0 @@
-#python acc_stream.py -b C1:07:9C:83:3C:CA -l /dev/null -sl testC1-s-$1.log -s $1 &
-#python acc_stream.py -b D0:D3:35:6C:42:38 -l /dev/null -sl testD0-s-$1.log -s $1 &
-#python acc_stream.py -b EA:54:B4:AE:83:D9 -l /dev/null -sl testEA-s-$1.log -s $1 &
-#python acc_stream.py -b DB:D7:F6:9C:72:06 -l /dev/null -sl testDB-s-$1.log -s $1 &
-python acc_stream.py -b F3:FD:50:BC:DC:40 -l /dev/null -sl testF3-s-$1.log -s $1 &
-
